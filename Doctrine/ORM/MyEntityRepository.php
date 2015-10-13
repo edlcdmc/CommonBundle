5f1ae4ad3ee2423517162f2baf7e1d4ff6b81c81
edlcdmc <edlcdmc@gmail.com>
2015-10-13 22:55:19 +0200

    sql injection - part2

--- Doctrine/ORM/MyEntityRepository.php
+++ Doctrine/ORM/MyEntityRepository.php
@@ -11,7 +11,7 @@ use Doctrine\ORM\QueryBuilder;
  * This class is designed for inheritance and users can subclass this class to
  * write their own repositories with business-specific methods to locate entities.
  *
- * @since   2.1
+ * @since   2.7
  * @author  Kamil Filipiak <edlcdmc@gmail.com>
  */
 class MyEntityRepository extends EntityRepository
@@ -26,50 +26,50 @@ class MyEntityRepository extends EntityRepository
 	public function findByMyCriteriaDQL($criteria, $sort) {
 		$qb = $this->createQueryBuilder('b')
 			->select('b');
-		// 		->leftJoin('b.parent', 'c')
-		// 		->where('b.active = 1' );
 		if(is_array($criteria)){
+			$i = 1;
 			foreach ($criteria as $name => $value){
-				$name = mysql_real_escape_string($name);
-				$value = mysql_real_escape_string($value);
-				$name_s = 'b.'.$name;
-				$qb->andWhere("$name_s = $value");
+				if($this->checkName($name) and $value) {
+					$name_s = 'b.'.$name;
+					$qb->andWhere("$name_s = :val$i");
+					$qb->setParameter("val$i", $value);
+					$i++;
+				}
 			}
 		}
-//    	if(is_array($sort)){
-//    		foreach ($sort as $name => $value){
-//    			$qb->addOrderBy('b.'.$name, $value);
-//    		}
-//    	}
 		if(is_array($sort)){
 			foreach ($sort as $name => $value){
 				$this->check($qb, $name, $value);
 			}
 		}
-		//	echo $qb->getDql();
-		//	->addOrderBy('b.createdAt', 'ASC');
-//    		$a = $qb->getDql();
-//    		echo $a;
 		return $qb->getQuery();
 	}
 
 	protected function check(QueryBuilder $qb, $name, $value, $letter = 'b.')
 	{
-		$name = mysql_real_escape_string($name);
-		$value = mysql_real_escape_string($value);
 		$value = strtolower($value);
-		$testName = false; $testValue = false;
-		foreach($this->_class->fieldMappings as $key => $object) {
-			if($name == $key) {
-				$testName = true;
-			}
-		}
+		$testValue = false;
 		if($value == 'asc' or $value == 'desc') {
 			$testValue = true;
 		}
-		if($testName and $testValue) {
+		if($this->checkName($name) and $testValue) {
 			$qb->addOrderBy($letter.$name, $value);
 			return true;
+		}
+		return false;
+	}
+
+	protected function checkName($name)
+	{
+		foreach($this->_class->fieldMappings as $key => $object) {
+			if($name == $key) {
+				return true;
+			}
+		}
+		foreach($this->_class->associationMappings as $key => $object) {
+			if($name == $key) {
+				return true;
+			}
 		}
 		return false;
 	}
